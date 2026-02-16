f62039bb77e58afa82353bef401cd911bf09d388
Pedro Vivas <pmvp82@gmail.com>
2026-02-16 18:41:34 -0500

    Chanhe Navbar styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,26 +3,26 @@ import { Link } from "react-router";
 
 export default function Navbar() {
   return (
-    <nav className="bg-primary backdrop-blur-md border-b sticky top-0 z-40">
-      <div className="max-w-7xl mx-auto px-6 h-20 flex items-center justify-between">
-        <Link to="/" className="flex items-center space-x-2 hover:opacity-50">
+    <nav className="bg-primary backdrop-blur-md border-b top-0 z-40">
+      <div className="max-w-7xl mx-auto px-6 h-30 flex items-center justify-between">
+        <Link to="/" className="flex items-center space-x-2 hover:opacity-75">
           <span className="w-16 h-16 rounded-xl flex items-center justify-center text-slate-900">
             <Home className="w-8 h-8" />
           </span>
-          <span className="text-3xl font-brand font-black tracking-tight text-slate-800">
-            Habitat Moderne inc.
+          <span className="font-brand font-black tracking-tight font-medium">
+            <span className="text-5xl">Habitat Moderne inc.</span>
             <br />
-            <span className="text-lg">Gestion immobilière</span>
+            <span className="text-3xl">Gestion immobilière</span>
           </span>
         </Link>
         <ul className="flex gap-3">
-          <li className="font-bold hover:opacity-50">
+          <li className="font-medium hover:opacity-75 text-lg">
             <Link to="/">Accueil</Link>
           </li>
-          <li className="font-bold hover:opacity-50">
+          <li className="font-medium hover:opacity-75 text-lg">
             <Link to="/">Nos appartements</Link>
           </li>
-          <li className="font-bold hover:opacity-50">
+          <li className="font-medium hover:opacity-75 text-lg">
             <Link to="/">Nous rejoindre</Link>
           </li>
         </ul>
