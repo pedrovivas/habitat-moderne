b543f28ef5b8f15193573ad84e1328ea35536c77
Pedro Vivas <pmvp82@gmail.com>
2026-02-16 20:19:49 -0500

    Remove Home icon from Navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,4 +1,3 @@
-import { Home } from "lucide-react";
 import { Link } from "react-router";
 
 export default function Navbar() {
@@ -6,9 +5,6 @@ export default function Navbar() {
     <nav className="bg-primary backdrop-blur-md border-b top-0 z-40">
       <div className="max-w-7xl mx-auto px-6 h-30 flex items-center justify-between">
         <Link to="/" className="flex items-center space-x-2 hover:opacity-75">
-          <span className="w-16 h-16 rounded-xl flex items-center justify-center text-slate-900">
-            <Home className="w-8 h-8" />
-          </span>
           <span className="font-brand font-black tracking-tight font-medium">
             <span className="text-5xl">Habitat Moderne inc.</span>
             <br />
