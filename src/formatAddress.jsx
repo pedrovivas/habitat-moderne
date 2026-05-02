ef223df229e15cc2679d17b8aaafef64285fdbbe
Pedro Vivas <pmvp82@gmail.com>
2026-05-02 13:04:36 -0400

    Remove city from formatAddress

--- src/formatAddress.jsx
+++ src/formatAddress.jsx
@@ -1,17 +1,17 @@
 import { useTranslation } from "react-i18next";
 
 export default function formatAddress(
-  { address, unit, neighborhood, city = "Montréal", postal_code },
+  { address, unit, neighborhood, postal_code },
   showNeighborhood = true,
 ) {
   const { t } = useTranslation();
 
-  const postalCodeFormatted = postal_code && `, QC ${postal_code}`;
+  const postalCodeFormatted = postal_code && `QC ${postal_code}`;
 
   const cityInfo =
     neighborhood && showNeighborhood
-      ? `${city}${postalCodeFormatted} (${neighborhood})`
-      : `${city}${postalCodeFormatted}`;
+      ? `${postalCodeFormatted} (${neighborhood})`
+      : `${postalCodeFormatted}`;
 
   return [address, unit && `${t("apartmentDetails.apartment")} ${unit}`, cityInfo].filter(Boolean).join(", ");
 }
